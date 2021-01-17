0b942bcd2a6bcdd5defba4a048f6cb76a0ed779c
Alec Savvy <alecsavvy@outlook.com>
2021-01-16 22:28:07 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import Chat from "./Chat";
+import Commands from "./Commands";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Snakey Bot</Typography>
+      </Toolbar>
+    </AppBar>
+    <Grid container>
+      <Grid item xs={8} style={{ padding: "1%" }}>
+        <Commands />
+      </Grid>
+      <Grid item xs={4}>
+        <Chat />
+      </Grid>
+    </Grid>
+  </div>
+);
 
 export default App;
---
+++ src/Styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    background: "C7AC92",
+  },
+});
+
+export default useStyles;
